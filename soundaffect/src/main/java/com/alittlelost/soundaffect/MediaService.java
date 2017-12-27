1615bf0b22550417b12dd20e152f2154700f5dd2
Sean Ashmore <alittlelost@gmail.com>
2017-12-27 23:34:59 +0000

    Adding play / pause actions to the notification

--- soundaffect/src/main/java/com/alittlelost/soundaffect/MediaService.java
+++ soundaffect/src/main/java/com/alittlelost/soundaffect/MediaService.java
@@ -3,6 +3,7 @@ package com.alittlelost.soundaffect;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -16,6 +17,9 @@ import android.support.annotation.Nullable;
  */
 
 public class MediaService extends Service {
+
+    public static final String ACTION_PAUSE = "ACTION_PAUSE";
+    public static final String ACTION_PLAY = "ACTION_PLAY";
 
     MediaManager mediaManager;
 
@@ -41,31 +45,52 @@ public class MediaService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Notification notification = null;
+        Notification.Builder notificationBuilder;
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel mChannel = new NotificationChannel("101", "Media-Notifications", NotificationManager.IMPORTANCE_LOW);
             notificationManager.createNotificationChannel(mChannel);
 
-            notification =
-                    new Notification.Builder(this, mChannel.getId())
-                            .setContentTitle("Title")
-                            .setContentText("Message")
-                            .setSmallIcon(R.drawable.ic_skip_previous_black_24dp)
-                            .setTicker("Ticker text")
-                            .build();
+            notificationBuilder =
+                    new Notification.Builder(this, mChannel.getId());
         } else {
-            notification =
-                    new Notification.Builder(this)
-                            .setContentTitle("Title")
-                            .setContentText("Message")
-                            .setSmallIcon(R.drawable.ic_skip_previous_black_24dp)
-                            .setTicker("Ticker text")
-                            .build();
+            notificationBuilder =
+                    new Notification.Builder(this);
         }
 
-        startForeground(101, notification);
+        if (intent.getAction() != null) {
+            if (intent.getAction().equals(ACTION_PAUSE)) {
+                if (mediaManager.isPlaying()) {
+                    mediaManager.pause();
+                }
+            }
+
+            if (intent.getAction().equals(ACTION_PLAY)) {
+                if (!mediaManager.isPlaying()) {
+                    mediaManager.play();
+                }
+            }
+        }
+
+
+        Intent pauseIntent = new Intent(this, MediaService.class);
+        pauseIntent.setAction(ACTION_PAUSE);
+        PendingIntent pausePendingIntent = PendingIntent.getService(this, 101, pauseIntent, 0);
+
+        Intent playIntent = new Intent(this, MediaService.class);
+        playIntent.setAction(ACTION_PLAY);
+        PendingIntent playPendingIntent = PendingIntent.getService(this, 101, playIntent, 0);
+
+
+        notificationBuilder.setContentTitle("Title")
+                .setContentText("Message")
+                .setSmallIcon(R.drawable.ic_skip_previous_black_24dp)
+                .setTicker("Ticker text")
+                .addAction(R.drawable.ic_pause_circle_outline_black_24dp, "Pause", pausePendingIntent)
+                .addAction(R.drawable.ic_play_circle_outline_black_24dp, "Play", playPendingIntent);
+
+        startForeground(101, notificationBuilder.build());
 
         return START_NOT_STICKY;
     }
